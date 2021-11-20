84e50f21c1e2200ccb867375e9d57bddcccef127
Victor-literate <94682385+Victor-literate@users.noreply.github.com>
2021-11-20 18:31:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(activeSlide) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[0].classList.add('active')
+    
+    for(const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+    
+        slide.classList.add('active')
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) =>{
+            slide.classList.remove('active')
+        })
+    }
+}
+
+slidesPlugin()
